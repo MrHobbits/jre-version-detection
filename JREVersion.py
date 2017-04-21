03e8e79eae6b492e59642868d557f40cbe760e7c
MrHobbits <ryancbarnes@gmail.com>
2017-04-21 01:14:41 -0400

    got my editor(s) working with Git. now I can get back to coding this.

--- JREVersion.py
+++ JREVersion.py
@@ -13,6 +13,7 @@ jre_version = BeautifulSoup(url_source,"lxml").find("a",{"name":"JDK8"}).text[8:
 
 print('Current Java Release is: {}'.format(jre_version))
 
+
 """
     TODO:
     1. Create functionality to store the last version we 'knew' about
@@ -22,10 +23,5 @@ print('Current Java Release is: {}'.format(jre_version))
     2. If the version is different, do something about it. Like launch
     a web browser or send an email, or post to slack or something.
 
-    3. Get this working with GIT.
 
-    4. Maybe get it working in PyCharm too for that added Git integration?
-
-    5. I dont like PyCharm as much as I thought I would (I dont like the colors)
-    and it looks funny. what about Atom.io? Lets see if it works with GIT.
 """
