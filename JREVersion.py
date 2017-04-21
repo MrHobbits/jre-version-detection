172e1018af83ca40b4fb137328fa2feadf52e5c8
MrHobbits <ryancbarnes@gmail.com>
2017-04-20 21:53:48 -0400

    Add readme.txt and JREVersion.py

---
+++ JREVersion.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+
+import requests
+
+# General downloads page, which leads to the JRE 8 download page
+url = "http://www.oracle.com/technetwork/java/javase/downloads/index.html"
+
+# grab the webpage in its entirety (source code)
+url_source = requests.get(url).text
+
+# make it beautiful
+jre_version = BeautifulSoup(url_source,"lxml").find("a",{"name":"JDK8"}).text[8:]
+
+print('Current Java Release is: {}'.format(jre_version))
+
+"""
+    TODO:
+    1. Create functionality to store the last version we 'knew' about
+    in a file and pull it open so we can examine if it's different
+    each time we run this.
+
+    2. If the version is different, do something about it. Like launch
+    a web browser or send an email, or post to slack or something.
+"""
