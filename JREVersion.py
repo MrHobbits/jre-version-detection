33cf22528d4d1d7848514e31369c8b43966d8940
MrHobbits <ryancbarnes@gmail.com>
2017-05-16 00:15:28 -0400

    General save, not a lot added at this time.

--- JREVersion.py
+++ JREVersion.py
@@ -1,20 +1,7 @@
-from bs4 import BeautifulSoup
-
-import requests
-
-# General downloads page, which leads to the JRE 8 download page
-url = "http://www.oracle.com/technetwork/java/javase/downloads/index.html"
-
-# grab the webpage in its entirety (source code)
-url_source = requests.get(url).text
-
-# make it beautiful
-jre_version = BeautifulSoup(url_source,"lxml").find("a",{"name":"JDK8"}).text[8:]
-
-print('Current Java Release is: {}'.format(jre_version))
-
+#!/usr/bin/python
 
 """
+
     TODO:
     1. Create functionality to store the last version we 'knew' about
     in a file and pull it open so we can examine if it's different
@@ -27,3 +14,46 @@ print('Current Java Release is: {}'.format(jre_version))
     for this and all future projects.
 
 """
+
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+import requests
+
+
+def last_known_version():
+    """
+    Loads the variables from the save file to compare to the version we find.
+    """
+    dat_file = open('javainfo.dat').read()
+
+    last_version = dat_file.split(chr(10))
+
+    right_now = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+    prev_time = datetime.strptime(last_version[1], "%Y-%m-%d %H:%M:%S")
+    now_time = datetime.strptime(right_now, "%Y-%m-%d %H:%M:%S")
+    time_diff = abs((now_time - prev_time).seconds)
+
+    return (last_version[0], time_diff)
+
+def get_version():
+    """
+    Attempts to grab the current version of Java from the oracle page
+    """
+
+    print(f"Old Version {last_known_version()[0]}")
+
+    # General downloads page, which leads to the JRE 8 download page
+    url = "http://www.oracle.com/technetwork/java/javase/downloads/index.html"
+
+    # grab the webpage in its entirety (source code)
+    url_source = requests.get(url).text
+
+    # make it beautiful
+    jre_version = BeautifulSoup(url_source, "lxml").find("a", {"name":"JDK8"}).text[8:]
+
+    return jre_version
+
+if __name__ == '__main__':
+    print(f"Latest version {get_version()}")
